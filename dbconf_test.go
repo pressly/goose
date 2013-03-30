b41a586b5fb8c29fea80172a519805e0c3d17f35
Liam Staskawicz <lstask@gmail.com>
2013-03-30 16:58:28 -0700

    dbconf: simple preliminary test

---
+++ dbconf_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	// "fmt"
+	"testing"
+)
+
+func TestBasics(t *testing.T) {
+
+	dbconf, err := makeDBConfDetails("db-sample", "test")
+	if err != nil {
+		t.Error("couldn't create DBConf")
+	}
+
+	got := []string{dbconf.MigrationsDir, dbconf.Env, dbconf.Driver, dbconf.OpenStr}
+	want := []string{"db-sample/migrations", "test", "postgres", "user=liam dbname=tester sslmode=disable"}
+
+	for i, s := range got {
+		if s != want[i] {
+			t.Errorf("Unexpected DBConf value. got %v, want %v", s, want[i])
+		}
+	}
+}
