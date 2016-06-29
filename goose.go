e4c76971238d65abceed67cd2478e99d79dfd165
Josh Fyne <josh.fyne@essencedigital.com>
2016-06-29 15:40:13 -0400

    Re-ordered create arguments, checked for missing create arguments

--- goose.go
+++ goose.go
@@ -16,7 +16,15 @@ func Run(command string, db *sql.DB, dir string, args ...string) error {
 			return err
 		}
 	case "create":
-		if err := Create(db, args[0], args[1], dir); err != nil {
+		if len(args) == 0 {
+			return fmt.Errorf("create must be of form: goose [OPTIONS] DRIVER DBSTRING create NAME [go|sql]")
+		}
+
+		migrationType := "go"
+		if len(args) == 2 {
+			migrationType = args[1]
+		}
+		if err := Create(db, dir, args[0], migrationType); err != nil {
 			return err
 		}
 	case "down":
