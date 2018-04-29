7f1e95103b032b15a410698ed121bd1864d49e81
Maxim Krasilnikov <mak.krasilnikov@gmail.com>
2018-04-29 14:11:02 +0300

    Added ability to change db version table name

--- migrate.go
+++ migrate.go
@@ -242,7 +242,7 @@ func EnsureDBVersion(db *sql.DB) (int64, error) {
 	return 0, ErrNoNextVersion
 }
 
-// Create the goose_db_version table
+// Create the db version table
 // and insert the initial 0 value into it
 func createVersionTable(db *sql.DB) error {
 	txn, err := db.Begin()
