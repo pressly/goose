0639337e0b110cbb32a342d9e9ad6edc3a13d067
Maxim Krasilnikov <mak.krasilnikov@gmail.com>
2018-04-29 14:11:02 +0300

    Added ability to change db version table name

--- version.go
+++ version.go
@@ -15,3 +15,15 @@ func Version(db *sql.DB, dir string) error {
 	log.Printf("goose: version %v\n", current)
 	return nil
 }
+
+var dbVersionTableName = "goose_db_version"
+
+// GetDBVersionTableName returns goose db version table name
+func GetDBVersionTableName() string {
+	return dbVersionTableName
+}
+
+// SetDBVersionTableName set goose db version table name
+func SetDBVersionTableName(n string) {
+	dbVersionTableName = n
+}
