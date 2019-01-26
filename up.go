7d3f706442475a311fe63b0d66641a05bf90b1eb
Elijah Carrel <ecarrel@grailbio.com>
2019-01-25 17:45:12 -0800

    Made including missing optional

--- up.go
+++ up.go
@@ -2,60 +2,51 @@ package goose
 
 import (
 	"database/sql"
+	"fmt"
 )
 
-// Up applies all available migrations.
-func Up(db *sql.DB, dir string) error {
-	return UpMissing(db, dir, false)
-}
-
-// UpByOne migrates up by a single version.
-func UpByOne(db *sql.DB, dir string) error {
-	return UpMissing(db, dir, true)
-}
-
-// UpMissing migrates all missing migrations
-func UpMissing(db *sql.DB, dir string, onlyOne bool) error {
-	migrations, err := MissingMigrations(db, dir)
-	if err != nil {
-		return err
+// Up performs all types of migrations upwards, depending on the params.
+func Up(db *sql.DB, dir string, includeMissing bool, onlyOne bool, endVersion *int64) error {
+	var migrations Migrations
+	if includeMissing {
+		var err error
+		migrations, err = MissingMigrations(db, dir)
+		if err != nil {
+			return err
+		}
+	} else {
+		current, err := GetDBVersion(db)
+		if err != nil {
+			return err
+		}
+		migrations, err = CollectMigrations(dir, current, maxVersion)
+		if err != nil {
+			return err
+		}
 	}
+	for {
+		current, err := GetDBVersion(db)
+		if err != nil {
+			return err
+		}
 
-	if len(migrations) == 0 {
-		log.Printf("goose: no migrations to run\n")
-	}
-
-	for _, migration := range migrations {
-		if err = migration.Up(db); err != nil {
+		next, err := migrations.Next(current)
+		if err != nil {
+			if err == ErrNoNextVersion {
+				log.Printf("goose: no migrations to run. current version: %d\n", current)
+				return nil
+			}
+			return err
+		}
+		if endVersion != nil && next.Version > *endVersion {
+			break
+		}
+		if err = next.Up(db); err != nil {
 			return err
 		}
 		if onlyOne {
 			break
 		}
 	}
-
-	return nil
-}
-
-// UpTo migrates up to a specific version.
-func UpTo(db *sql.DB, dir string, version int64) error {
-	migrations, err := MissingMigrations(db, dir)
-	if err != nil {
-		return err
-	}
-
-	if len(migrations) == 0 {
-		log.Printf("goose: no migrations to run\n")
-	}
-
-	for _, migration := range migrations {
-		if migration.Version > version {
-			break
-		}
-		if err = migration.Up(db); err != nil {
-			return err
-		}
-	}
-
-	return nil
+	return fmt.Errorf("should not happen")
 }
