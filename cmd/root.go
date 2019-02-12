39273661a98a106fcd78ad8894c460f33ad3c2fb
Patrick Arnold <patrick.a@geniusmonkey.com>
2019-02-11 21:32:00 -0700

    update root command description

--- cmd/root.go
+++ cmd/root.go
@@ -20,7 +20,7 @@ var db *sql.DB
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "goose",
-	Short: "CLI for extracting data and load bacon",
+	Short: "CLI for running SQL migrations",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
