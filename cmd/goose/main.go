pressly/goose

commit f3abc85fac039b50519facf4ff32f99ec27bf143

allow to specify migrations table

cmd/goose/main.go
@@ -11,7 +11,8 @@ import (
 
 var (
 	flags   = flag.NewFlagSet("goose", flag.ExitOnError)
-	dir     = flags.String("dir", ".", "directory with migration files")
+	dir     = flags.String("dir", "", "directory with migration files")
+	table   = flags.String("table", "goose_db_version", "migrations table name")
 	verbose = flags.Bool("v", false, "enable verbose mode")
 	help    = flags.Bool("h", false, "print help")
 	version = flags.Bool("version", false, "print version")
@@ -28,6 +29,7 @@ func main() {
 	if *verbose {
 		goose.SetVerbose(true)
 	}
+	goose.SetTableName(*table)
 
 	args := flags.Args()
 	if len(args) == 0 || *help {

commit 93314d45057275b32da1d2f05747feae5609a1be

Fixes #209 (regression from f3abc85f): Add default -dir value

cmd/goose/main.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	flags   = flag.NewFlagSet("goose", flag.ExitOnError)
-	dir     = flags.String("dir", "", "directory with migration files")
+	dir     = flags.String("dir", ".", "directory with migration files")
 	table   = flags.String("table", "goose_db_version", "migrations table name")
 	verbose = flags.Bool("v", false, "enable verbose mode")
 	help    = flags.Bool("h", false, "print help")