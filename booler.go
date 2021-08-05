ee6b4eeed315d9ecf9a355466ba882720211e0f4
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2021-08-05 20:49:16 +1000

    booler test

--- booler.go
+++ booler.go
@@ -4,12 +4,16 @@ import (
 	"database/sql/driver"
 	"fmt"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type booler bool
 
 func (b *booler) Scan(src interface{}) error {
 	switch x := src.(type) {
+	case bool:
+		*b = booler(x)
 	case int:
 		*b = x == 1
 	case int64:
@@ -17,8 +21,11 @@ func (b *booler) Scan(src interface{}) error {
 	case string:
 		v := strings.ToLower(x)
 		*b = (v == "y") || (v == "yes") || (v == "t") || (v == "true")
-	case bool:
-		*b = booler(x)
+	case []uint8:
+		if len(x) != 1 {
+			return errors.New("invalid []uint8")
+		}
+		*b = x[0] == 1
 	default:
 		return fmt.Errorf("unknown scanner source %T", src)
 	}
---
+++ booler_test.go
@@ -0,0 +1,71 @@
+package goose
+
+import (
+	"database/sql/driver"
+	"reflect"
+	"testing"
+)
+
+func Test_booler_Scan(t *testing.T) {
+	type args struct {
+		src interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    booler
+		wantErr bool
+	}{
+		{"bool_t", args{true}, booler(true), false},
+		{"bool_f", args{false}, booler(false), false},
+		{"byte_0", args{[]byte{0}}, booler(false), false},
+		{"byte_1", args{[]byte{1}}, booler(true), false},
+		{"byte_long", args{[]byte("too_long")}, booler(false), true},
+		{"int_2", args{2}, booler(false), false},
+		{"int_1", args{1}, booler(true), false},
+		{"int_0", args{0}, booler(false), false},
+		{"str_y", args{"y"}, booler(true), false},
+		{"str_Y", args{"Y"}, booler(true), false},
+		{"str_t", args{"t"}, booler(true), false},
+		{"str_yes", args{"yes"}, booler(true), false},
+		{"str_true", args{"true"}, booler(true), false},
+		{"str_other", args{"x"}, booler(false), false},
+		{"str_not_yes", args{"yas"}, booler(false), false},
+		{"invalid type", args{float32(4.2)}, booler(false), true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var b booler
+			if err := b.Scan(tt.args.src); (err != nil) != tt.wantErr {
+				t.Errorf("Scan() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if bool(b) != bool(tt.want) {
+				t.Errorf("Scan() want=%v, got=%v", tt.want, b)
+			}
+		})
+	}
+}
+
+func Test_booler_Value(t *testing.T) {
+	tests := []struct {
+		name    string
+		b       booler
+		want    driver.Value
+		wantErr bool
+	}{
+		{"true", booler(true), driver.Value("Y"), false},
+		{"true", booler(false), driver.Value("N"), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.b.Value()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Value() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Value() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
