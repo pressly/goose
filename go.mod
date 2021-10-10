8c489a012c1cdedbfa2d39801313e9b47ddf8589
Mike Fridman <mf192@icloud.com>
2021-10-09 21:58:25 -0400

    Try out minimal assertion package matryer/is

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/denisenkom/go-mssqldb v0.10.0
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/lib/pq v1.10.2
+	github.com/matryer/is v1.4.0
 	github.com/mattn/go-sqlite3 v1.14.8
 	github.com/ory/dockertest/v3 v3.8.0
 	github.com/pkg/errors v0.9.1
--- tests/migrations_test.go
+++ tests/migrations_test.go
@@ -6,57 +6,42 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/matryer/is"
 	"github.com/pressly/goose/v3"
 )
 
 func TestMigrateUp(t *testing.T) {
-	db, err := newDockerDatabase(t, dialectPostgres, 5432)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Parallel()
+	is := is.New(t)
+
+	db, err := newDockerDatabase(t, dialectPostgres, 0)
+	is.NoErr(err)
 	goose.SetDialect(dialectPostgres)
 	migrationsDir := filepath.Join("testdata", "migrations")
 	migrations, err := goose.CollectMigrations(migrationsDir, 0, goose.MaxVersion)
-	if err != nil {
-		t.Fatal(err)
-	}
+	is.NoErr(err)
 	numberOfMigrations := len(migrations)
-	if numberOfMigrations == 0 {
-		t.Fatal("must supply valid migrations")
-	}
+	is.True(numberOfMigrations != 0)
 	// Migrate all
-	if err := goose.Up(db, migrationsDir); err != nil {
-		t.Fatal(err)
-	}
+	err = goose.Up(db, migrationsDir)
+	is.NoErr(err)
 	currentVersion, err := goose.GetDBVersion(db)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if want := migrations[len(migrations)-1].Version; currentVersion != want {
-		t.Fatalf("failed to match current vesion:%d with migration file max version:%d",
-			currentVersion,
-			want,
-		)
-	}
+	is.NoErr(err)
+	is.Equal(currentVersion, migrations[len(migrations)-1].Version)
 	// Validate the version actually matches what goose claims it is .
-	row := db.QueryRow(fmt.Sprintf("select max(version_id) from %s", goose.TableName()))
 	var gotVersion int
-	if err := row.Scan(&gotVersion); err != nil {
-		t.Fatal(err)
-	}
-	if gotVersion != numberOfMigrations {
-		t.Fatalf("failed to match db version with migration number from dir: got %d want %d",
-			gotVersion,
-			numberOfMigrations,
-		)
-	}
+	err = db.QueryRow(
+		fmt.Sprintf("select max(version_id) from %s", goose.TableName()),
+	).Scan(&gotVersion)
+	is.NoErr(err)
+	is.Equal(gotVersion, numberOfMigrations) // incorrect database version.
 }
 
 func TestMigrateUpTo(t *testing.T) {
 	const (
 		upToVersion = 2
 	)
-	db, err := newDockerDatabase(t, dialectPostgres, 5432)
+	db, err := newDockerDatabase(t, dialectPostgres, 0)
 	if err != nil {
 		t.Fatal(err)
 	}
