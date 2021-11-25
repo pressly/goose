71a29d3730b68434251399ebdf6ae981ec6a14aa
Michael Fridman <mf192@icloud.com>
2021-11-24 23:09:35 -0500

    fix driver name

--- goose_test.go
+++ goose_test.go
@@ -122,7 +122,7 @@ var migrations embed.FS
 
 func TestEmbeddedMigrations(t *testing.T) {
 	// not using t.Parallel here to avoid races
-	db, err := sql.Open("sqlite3", "sql_embed.db")
+	db, err := sql.Open("sqlite", "sql_embed.db")
 	if err != nil {
 		t.Fatalf("Database open failed: %s", err)
 	}
