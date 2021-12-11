c60c223bed518eaf59ee3461a627929130588011
Michael Fridman <mf192@icloud.com>
2021-12-11 14:58:57 -0500

    Add a few more migration files

--- tests/e2e/testdata/postgres/migrations/00002_b.sql
+++ tests/e2e/testdata/postgres/migrations/00002_b.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 INSERT INTO owners(owner_id, owner_name, owner_type) 
-    VALUES (1, 'lucas', 'user'), (2, 'spacey', 'organization');
+    VALUES (1, 'lucas', 'user'), (2, 'space', 'organization');
 -- +goose StatementEnd
 
 -- +goose Down
---
+++ tests/e2e/testdata/postgres/migrations/00010_j.sql
@@ -0,0 +1,6 @@
+-- +goose NO TRANSACTION
+-- +goose Up
+DROP INDEX IF EXISTS owners_owner_name_idx;
+
+-- +goose Down
+CREATE UNIQUE INDEX CONCURRENTLY ON owners(owner_name);
