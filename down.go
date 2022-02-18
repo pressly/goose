d8330106064e88dc5cb21de418ac8a271921d1f0
Schparky <3172830+Schparky@users.noreply.github.com>
2022-02-17 22:43:53 -0700

    DownTo returns error if no matching migration file is found

--- down.go
+++ down.go
@@ -56,8 +56,12 @@ func DownTo(db *sql.DB, dir string, version int64, opts ...OptionsFunc) error {
 
 		current, err := migrations.Current(currentVersion)
 		if err != nil {
-			log.Printf("goose: no migrations to run. current version: %d\n", currentVersion)
-			return nil
+			if currentVersion == 0 {
+				log.Printf("goose: no migrations to run. current version: %d\n", currentVersion)
+				return nil
+			}
+			log.Printf("goose: migration file not found for current version (%d), error: %s,\n", currentVersion, err)
+			return err
 		}
 
 		if current.Version <= version {
