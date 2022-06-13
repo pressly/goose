ee67086524546ab742ed16d7f7328d8049dddb89
Petr Korobeinikov <pkorobeinikov@gmail.com>
2022-06-13 17:29:41 +0300

    Len logic fix (#336)

--- migrate.go
+++ migrate.go
@@ -154,7 +154,7 @@ func collectMigrationsFS(fsys fs.FS, dirpath string, current, target int64) (Mig
 		return nil, err
 	}
 
-	if len(sqlMigrationFiles) == 0 || len(registeredGoMigrations) == 0 {
+	if len(sqlMigrationFiles) == 0 && len(registeredGoMigrations) == 0 {
 		return nil, fmt.Errorf("%s directory does not contain any kind of migration files", dirpath)
 	}
 
