c45627a3c4946b9ac9795debd8d48834dbe6e01d
Michael Fridman <mf192@icloud.com>
2022-06-20 21:06:38 -0400

    named vars

--- internal/testdb/testdb.go
+++ internal/testdb/testdb.go
@@ -5,20 +5,20 @@ import "database/sql"
 // NewClickHouse starts a ClickHouse docker container, and returns
 // a connection and a cleanup function.
 // If bindPort is 0, a random port will be used.
-func NewClickHouse(options ...OptionsFunc) (_ *sql.DB, cleanup func(), _ error) {
+func NewClickHouse(options ...OptionsFunc) (db *sql.DB, cleanup func(), err error) {
 	return newClickHouse(options...)
 }
 
 // NewPostgres starts a Postgre docker container, and returns
 // a connection and a cleanup function.
 // If bindPort is 0, a random port will be used.
-func NewPostgres(options ...OptionsFunc) (_ *sql.DB, cleanup func(), _ error) {
+func NewPostgres(options ...OptionsFunc) (db *sql.DB, cleanup func(), err error) {
 	return newPostgres(options...)
 }
 
 // NewMariaDB starts a MariaDB docker container, and returns
 // a connection and a cleanup function.
 // If bindPort is 0, a random port will be used.
-func NewMariaDB(options ...OptionsFunc) (_ *sql.DB, cleanup func(), _ error) {
+func NewMariaDB(options ...OptionsFunc) (db *sql.DB, cleanup func(), err error) {
 	return newMariaDB(options...)
 }
