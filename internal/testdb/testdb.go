d04c088d096a40cce3e20a30b286aacc957ac9f2
Michael Fridman <mf192@icloud.com>
2022-06-20 21:42:33 -0400

    e2e: move db container helpers to internal/testdb (#371)

--- internal/testdb/testdb.go
+++ internal/testdb/testdb.go
@@ -2,9 +2,17 @@ package testdb
 
 import "database/sql"
 
-// NewClickHouse starts a ClickHouse docker container, and returns
-// a connection and a cleanup function.
-// If bindPort is 0,b  a random port will be used.
-func NewClickHouse(options ...OptionsFunc) (_ *sql.DB, cleanup func(), _ error) {
+// NewClickHouse starts a ClickHouse docker container. Returns db connection and a docker cleanup function.
+func NewClickHouse(options ...OptionsFunc) (db *sql.DB, cleanup func(), err error) {
 	return newClickHouse(options...)
+}
+
+// NewPostgres starts a PostgreSQL docker container. Returns db connection and a docker cleanup function.
+func NewPostgres(options ...OptionsFunc) (db *sql.DB, cleanup func(), err error) {
+	return newPostgres(options...)
+}
+
+// NewMariaDB starts a MariaDB docker container. Returns a db connection and a docker cleanup function.
+func NewMariaDB(options ...OptionsFunc) (db *sql.DB, cleanup func(), err error) {
+	return newMariaDB(options...)
 }
