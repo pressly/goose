2d0d6ebdfb19476c401a0de15708c46a840f39e7
Mike Fridman <mf192@icloud.com>
2022-06-23 08:29:25 -0400

    wip

--- provider.go
+++ provider.go
@@ -3,10 +3,14 @@ package goose
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"io/fs"
-
-	"github.com/pkg/errors"
+	"path"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 const (
@@ -50,7 +54,49 @@ func NewProvider(driverName, dbString, dir string, opts ...OptionsFunc) (*Provid
 }
 
 func collectMigrations(fsys fs.FS, dir string) (Migrations, error) {
-	return nil, nil
+	var migrations Migrations
+
+	// Collect all SQL migration files.
+	sqlMigrationFiles, err := fs.Glob(fsys, path.Join(dir, "*.sql"))
+	if err != nil {
+		return nil, err
+	}
+	sort.Strings(sqlMigrationFiles)
+	for _, fileName := range sqlMigrationFiles {
+		version, err := parseNumericComponent(fileName)
+		if err != nil {
+			return nil, err
+		}
+		migrations = append(migrations, &Migration{
+			Source:   fileName,
+			Version:  version,
+			Next:     -1,
+			Previous: -1,
+		})
+	}
+	return migrations, nil
+}
+
+func parseNumericComponent(name string) (int64, error) {
+	base := filepath.Base(name)
+	// https://github.com/pressly/goose/issues/331#issuecomment-1101556360
+	// Should we silently ignore non .sql and .go files ?
+	// Should we add -ignore or -exclude flags?
+	if ext := filepath.Ext(base); ext != ".go" && ext != ".sql" {
+		return 0, errors.New("migration file does not have .sql or .go file extension")
+	}
+	idx := strings.Index(base, "_")
+	if idx < 0 {
+		return 0, errors.New("no filename separator '_' found")
+	}
+	n, err := strconv.ParseInt(base[:idx], 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("failed to parse version: %w", err)
+	}
+	if n < 1 {
+		return 0, errors.New("migration version must be greater than zero")
+	}
+	return n, nil
 }
 
 func (p *Provider) Up(ctx context.Context)      {}
