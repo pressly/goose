e3b69a818328d485e293ab25a508071988c298e6
Craig Pastro <pastro.craig@gmail.com>
2022-07-09 17:04:25 -0700

    Ensure postgres is mapped to pgx in main

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -86,12 +86,14 @@ func main() {
 	}
 
 	driver, dbstring, command := args[0], args[1], args[2]
-	// To avoid breaking existing consumers, treat sqlite3 as sqlite.
-	// An implementation detail that consumers should not care which
-	// underlying driver is used. Internally uses the CGo-free port
-	// of SQLite: modernc.org/sqlite
+	// To avoid breaking existing consumers. An implementation detail
+	// that consumers should not care which underlying driver is used.
 	if driver == "sqlite3" {
+		//  Internally uses the CGo-free port of SQLite: modernc.org/sqlite
 		driver = "sqlite"
+	}
+	if driver == "postgres" {
+		driver = "pgx"
 	}
 	db, err := goose.OpenDBWithDriver(driver, normalizeDBString(driver, dbstring, *certfile, *sslcert, *sslkey))
 	if err != nil {
