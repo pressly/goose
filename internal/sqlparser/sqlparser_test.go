f549b42f38b6a287f0fa1ef83480874a268b4404
Michael Fridman <mf192@icloud.com>
2022-07-14 09:42:58 -0400

    move files (tmp)

--- internal/sqlparser/sqlparser_test.go
+++ internal/sqlparser/sqlparser_test.go
@@ -107,9 +107,9 @@ func TestUseTransactions(t *testing.T) {
 	}
 
 	tests := []testData{
-		{fileName: "./examples/sql-migrations/00001_create_users_table.sql", useTransactions: true},
-		{fileName: "./examples/sql-migrations/00002_rename_root.sql", useTransactions: true},
-		{fileName: "./examples/sql-migrations/00003_no_transaction.sql", useTransactions: false},
+		{fileName: "testdata/valid-txn/00001_create_users_table.sql", useTransactions: true},
+		{fileName: "testdata/valid-txn/00002_rename_root.sql", useTransactions: true},
+		{fileName: "testdata/valid-txn/00003_no_transaction.sql", useTransactions: false},
 	}
 
 	for _, test := range tests {
