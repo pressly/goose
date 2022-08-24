ad39ad5e501435432264bc22df7c18b3c1726c8b
egor.lyutov <egor.lyutov@instamart.ru>
2022-08-24 18:11:00 +0500

    exit with error 1 without required arguments

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -51,10 +51,17 @@ func main() {
 	goose.SetTableName(*table)
 
 	args := flags.Args()
-	if len(args) == 0 || *help {
+
+	if *help {
 		flags.Usage()
 		return
 	}
+
+	if len(args) == 0 {
+		flags.Usage()
+		os.Exit(1)
+	}
+
 	// The -dir option has not been set, check whether the env variable is set
 	// before defaulting to ".".
 	if *dir == defaultMigrationDir && os.Getenv(envGooseMigrationDir) != "" {
