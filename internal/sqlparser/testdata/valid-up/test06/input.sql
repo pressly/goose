4d3b8dc9c588e8a3d3f8c7503e97ed0602d9d9a8
Mike Fridman <mf192@icloud.com>
2023-01-24 08:54:14 -0500

    simplify empty lines test

--- internal/sqlparser/testdata/valid-up/test06/input.sql
+++ internal/sqlparser/testdata/valid-up/test06/input.sql
@@ -49,5 +49,14 @@ $$ LANGUAGE plpgsql;
 
 -- +goose StatementEnd
 
+INSERT INTO post (id, title, body)
+VALUES ('id_01', 'my_title', '
+this is an insert statement including empty lines.
+
+empty (blank) lines can be meaningful.
+
+leave the lines to keep the text syntax.
+');
 
 -- +goose Down
+TRUNCATE TABLE post; 
