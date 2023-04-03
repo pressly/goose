9649235232d5c7d1be3835ad3dd2fa3441730acd
Mike Fridman <mf192@icloud.com>
2023-04-02 21:18:01 -0400

    Update redo comment

--- redo.go
+++ redo.go
@@ -8,8 +8,11 @@ type RedoResult struct {
 }
 
 // Redo rolls back the most recently applied migration, then runs it again.
+//
+// Important, it is not safe to run this function concurrently with other goose functions.
 func (p *Provider) Redo(ctx context.Context) (*RedoResult, error) {
-	// feat(mf): lock the database to prevent concurrent migrations.
+	// feat(mf): lock the database to prevent concurrent migrations. Each of these functions should
+	// be run within the same lock?
 	downResult, err := p.Down(ctx)
 	if err != nil {
 		return nil, err
