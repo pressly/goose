7ce30b743d8621d16e3c8ef7d7227b499ba7d0ff
Michael Fridman <mf192@icloud.com>
2023-05-05 08:35:06 -0400

    docs: add goose validate command to help and readme (#512)

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -241,6 +241,7 @@ Commands:
     version              Print the current version of the database
     create NAME [sql|go] Creates new migration file with the current timestamp
     fix                  Apply sequential ordering to migrations
+    validate             Check migration files without running them
 `
 )
 
