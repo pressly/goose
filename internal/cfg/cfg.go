51168fddcf9ae276a1a8034285e2b879f94f1719
Arun Sori <asori@gitlab.com>
2023-05-16 12:10:44 +0200

    Add support for replicated state table in ClickHouse + Fix ListMigrations in ClickHouse

--- internal/cfg/cfg.go
+++ internal/cfg/cfg.go
@@ -2,7 +2,6 @@ package cfg
 
 import (
 	"os"
-	"strings"
 )
 
 var (
@@ -11,8 +10,6 @@ var (
 	GOOSEMIGRATIONDIR = envOr("GOOSE_MIGRATION_DIR", DefaultMigrationDir)
 	// https://no-color.org/
 	GOOSENOCOLOR = envOr("NO_COLOR", "false")
-
-	GOOSE_CLICKHOUSE_PARAMS = envOr("GOOSE_CLICKHOUSE_PARAMS", "")
 )
 
 var (
@@ -31,7 +28,6 @@ func List() []EnvVar {
 		{Name: "GOOSE_DBSTRING", Value: GOOSEDBSTRING},
 		{Name: "GOOSE_MIGRATION_DIR", Value: GOOSEMIGRATIONDIR},
 		{Name: "NO_COLOR", Value: GOOSENOCOLOR},
-		{Name: "GOOSE_CLICKHOUSE_PARAMS", Value: GOOSE_CLICKHOUSE_PARAMS},
 	}
 }
 
@@ -42,18 +38,4 @@ func envOr(key, def string) string {
 		val = def
 	}
 	return val
-}
-
-func SplitKeyValuesIntoMap(input string) map[string]string {
-	cutset := strings.Split(input, ",")
-
-	options := make(map[string]string)
-	for _, item := range cutset {
-		keyAndValues := strings.Split(item, "=")
-		if len(keyAndValues) != 2 {
-			continue
-		}
-		options[keyAndValues[0]] = keyAndValues[1]
-	}
-	return options
 }
