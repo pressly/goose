eb00ac25fe1e56b3cbd68c15ac3f38917e68abc2
Mike Fridman <mf192@icloud.com>
2023-05-28 15:59:25 -0400

    Improve comment

--- version.go
+++ version.go
@@ -5,7 +5,8 @@ import (
 	"errors"
 )
 
-// GetDBVersion retrieves the current database version.
+// GetDBVersion retrieves the max version applied to the database, regardless of when it was
+// applied. If no migrations have been applied, it returns 0.
 //
 // It is safe for concurrent use.
 func (p *Provider) GetDBVersion(ctx context.Context) (_ int64, retErr error) {
