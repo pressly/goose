158b8504faf66c70bbe01cfe3a2053c3674a9598
Mike Fridman <mf192@icloud.com>
2023-10-06 23:32:19 -0400

    refactor Provider and add tests

--- provider.go
+++ provider.go
@@ -4,17 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"io/fs"
 	"time"
 
 	"github.com/pressly/goose/v3/internal/sqladapter"
 )
-
-// Provider is a goose migration provider.
-type Provider struct {
-	db    *sql.DB
-	store sqladapter.Store
-	opt   *ProviderOptions
-}
 
 // NewProvider returns a new goose Provider.
 //
@@ -22,57 +16,84 @@ type Provider struct {
 // example, if the database dialect is "postgres", the database/sql driver could be
 // github.com/lib/pq or github.com/jackc/pgx.
 //
-// If opts is nil, the default options are used. See ProviderOptions for more information.
+// fsys is the filesystem used to read the migration files. Most users will want to use
+// os.DirFS("path/to/migrations") to read migrations from the local filesystem. However, it is
+// possible to use a different filesystem, such as embed.FS.
+//
+// Functional options are used to configure the Provider. See [ProviderOption] for more information.
 //
 // Unless otherwise specified, all methods on Provider are safe for concurrent use.
-func NewProvider(dialect Dialect, db *sql.DB, opts *ProviderOptions) (*Provider, error) {
+func NewProvider(
+	dialect Dialect,
+	db *sql.DB,
+	fsys fs.FS,
+	opts ...ProviderOption,
+) (*Provider, error) {
 	if db == nil {
 		return nil, errors.New("db must not be nil")
 	}
 	if dialect == "" {
 		return nil, errors.New("dialect must not be empty")
 	}
-	if opts == nil {
-		opts = DefaultOptions()
+	if fsys == nil {
+		return nil, errors.New("fsys must not be nil")
 	}
-	if err := validateOptions(opts); err != nil {
-		return nil, err
+	var cfg config
+	for _, opt := range opts {
+		if err := opt.apply(&cfg); err != nil {
+			return nil, err
+		}
 	}
-	store, err := sqladapter.NewStore(string(dialect), opts.Tablename)
+	// Set defaults
+	if cfg.tableName == "" {
+		cfg.tableName = defaultTablename
+	}
+	store, err := sqladapter.NewStore(string(dialect), cfg.tableName)
 	if err != nil {
 		return nil, err
 	}
-	// TODO(mf): implement the rest of this function
-	// - collect sources
-	// - merge sources into migrations
+	// TODO(mf): implement the rest of this function - collect sources - merge sources into
+	// migrations
 	return &Provider{
 		db:    db,
+		fsys:  fsys,
+		cfg:   cfg,
 		store: store,
-		opt:   opts,
 	}, nil
+}
+
+// Provider is a goose migration provider.
+type Provider struct {
+	db    *sql.DB
+	fsys  fs.FS
+	cfg   config
+	store sqladapter.Store
 }
 
 // MigrationStatus represents the status of a single migration.
 type MigrationStatus struct {
-	Applied   bool
+	// State represents the state of the migration. One of "untracked", "pending", "applied".
+	//  - untracked: in the database, but not on the filesystem.
+	//  - pending: on the filesystem, but not in the database.
+	//  - applied: in both the database and on the filesystem.
+	State string
+	// AppliedAt is the time the migration was applied. Only set if state is applied or untracked.
 	AppliedAt time.Time
-	Source    Source
+	// Source is the migration source. Only set if the state is pending or applied.
+	Source Source
 }
 
-// StatusOptions represents options for the Status method.
-type StatusOptions struct{}
-
-// Status returns the status of all migrations. The returned slice is ordered by ascending version.
-//
-// If opts is nil, the default options are used. See StatusOptions for more information.
-func (p *Provider) Status(ctx context.Context, opts *StatusOptions) ([]*MigrationStatus, error) {
+// Status returns the status of all migrations, merging the list of migrations from the database and
+// filesystem. The returned items are ordered by version, in ascending order.
+func (p *Provider) Status(ctx context.Context) ([]*MigrationStatus, error) {
 	return nil, errors.New("not implemented")
 }
 
-// GetDBVersion returns the max version from the database, regardless of when it was applied. If no
-// migrations have been applied, it returns 0.
+// GetDBVersion returns the max version from the database, regardless of the applied order. For
+// example, if migrations 1,4,2,3 were applied, this method returns 4. If no migrations have been
+// applied, it returns 0.
 func (p *Provider) GetDBVersion(ctx context.Context) (int64, error) {
-	return -1, errors.New("not implemented")
+	return 0, errors.New("not implemented")
 }
 
 // SourceType represents the type of migration source.
@@ -120,23 +141,23 @@ func (p *Provider) Close() error {
 type MigrationResult struct{}
 
 // ApplyVersion applies exactly one migration at the specified version. If there is no source for
-// the specified version, this method returns ErrNoCurrentVersion. If the migration has been applied
-// already, this method returns ErrAlreadyApplied.
+// the specified version, this method returns [ErrNoCurrentVersion]. If the migration has been
+// applied already, this method returns [ErrAlreadyApplied].
 //
-// If direction is true, the "up" migration is applied. If direction is false, the "down" migration
-// is applied.
+// When direction is true, the up migration is executed, and when direction is false, the down
+// migration is executed.
 func (p *Provider) ApplyVersion(ctx context.Context, version int64, direction bool) (*MigrationResult, error) {
 	return nil, errors.New("not implemented")
 }
 
-// Up applies all new migrations. If there are no new migrations to apply, this method returns empty
-// list and nil error.
+// Up applies all pending migrations. If there are no new migrations to apply, this method returns
+// empty list and nil error.
 func (p *Provider) Up(ctx context.Context) ([]*MigrationResult, error) {
 	return nil, errors.New("not implemented")
 }
 
 // UpByOne applies the next available migration. If there are no migrations to apply, this method
-// returns ErrNoMigrations.
+// returns [ErrNoNextVersion].
 func (p *Provider) UpByOne(ctx context.Context) (*MigrationResult, error) {
 	return nil, errors.New("not implemented")
 }
@@ -144,22 +165,22 @@ func (p *Provider) UpByOne(ctx context.Context) (*MigrationResult, error) {
 // UpTo applies all available migrations up to and including the specified version. If there are no
 // migrations to apply, this method returns empty list and nil error.
 //
-// For example, suppose there are 3 new migrations available 9,10,11. The current database version
-// is 8 and the requested version is 10. In this scenario only versions 9,10 will be applied.
+// For instance, if there are three new migrations (9,10,11) and the current database version is 8
+// with a requested version of 10, only versions 9 and 10 will be applied.
 func (p *Provider) UpTo(ctx context.Context, version int64) ([]*MigrationResult, error) {
 	return nil, errors.New("not implemented")
 }
 
 // Down rolls back the most recently applied migration. If there are no migrations to apply, this
-// method returns ErrNoMigrations.
+// method returns [ErrNoNextVersion].
 func (p *Provider) Down(ctx context.Context) (*MigrationResult, error) {
 	return nil, errors.New("not implemented")
 }
 
 // DownTo rolls back all migrations down to but not including the specified version.
 //
-// For example, suppose the current database version is 11, and the requested version is 9. In this
-// scenario only migrations 11 and 10 will be rolled back.
+// For instance, if the current database version is 11, and the requested version is 9, only
+// migrations 11 and 10 will be rolled back.
 func (p *Provider) DownTo(ctx context.Context, version int64) ([]*MigrationResult, error) {
 	return nil, errors.New("not implemented")
 }
--- provider_options.go
+++ provider_options.go
@@ -1,55 +1,50 @@
 package goose
 
 import (
+	"errors"
 	"fmt"
-	"io/fs"
 )
 
 const (
 	defaultTablename = "goose_db_version"
-	defaultDir       = "migrations"
 )
 
-// ProviderOptions is used to configure a Provider.
-type ProviderOptions struct {
-	// Dir is the directory where the migration files are located.
-	//
-	// Default: "migrations"
-	Dir string
-	// Tablename is the name of the database table used to track history of applied migrations.
-	//
-	// Default: "goose_db_version"
-	Tablename string
-	// Filesystem is the filesystem used to read the migration files.
-	// Required field.
-	//
-	// Default: read from disk
-	Filesystem fs.FS
-
-	Verbose bool
-
-	// Features
-	AllowMissing bool
+// ProviderOption is a configuration option for a goose provider.
+type ProviderOption interface {
+	apply(*config) error
 }
 
-// DefaultOptions returns the default ProviderOptions.
-func DefaultOptions() *ProviderOptions {
-	return &ProviderOptions{
-		Dir:        defaultDir,
-		Tablename:  defaultTablename,
-		Filesystem: osFS{},
-	}
+// WithTableName sets the name of the database table used to track history of applied migrations.
+//
+// If WithTableName is not called, the default value is "goose_db_version".
+func WithTableName(name string) ProviderOption {
+	return configFunc(func(c *config) error {
+		if c.tableName != "" {
+			return fmt.Errorf("table already set to %q", c.tableName)
+		}
+		if name == "" {
+			return errors.New("table must not be empty")
+		}
+		c.tableName = name
+		return nil
+	})
 }
 
-func validateOptions(opts *ProviderOptions) error {
-	if opts.Dir == "" {
-		return fmt.Errorf("dir must not be empty")
-	}
-	if opts.Tablename == "" {
-		return fmt.Errorf("table must not be empty")
-	}
-	if opts.Filesystem == nil {
-		return fmt.Errorf("filesystem must not be nil")
-	}
-	return nil
+// WithVerbose enables verbose logging.
+func WithVerbose() ProviderOption {
+	return configFunc(func(c *config) error {
+		c.verbose = true
+		return nil
+	})
+}
+
+type config struct {
+	tableName string
+	verbose   bool
+}
+
+type configFunc func(*config) error
+
+func (o configFunc) apply(cfg *config) error {
+	return o(cfg)
 }
