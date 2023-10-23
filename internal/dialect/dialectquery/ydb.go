b076cc5c7f8181c4cdd77d4980836860eb5d8080
Aleksey Myasnikov <asmyasnikov@yandex-team.com>
2023-10-23 18:05:43 +0300

    fix sql issues from mfridman

--- internal/dialect/dialectquery/ydb.go
+++ internal/dialect/dialectquery/ydb.go
@@ -7,56 +7,41 @@ type Ydb struct{}
 var _ Querier = (*Ydb)(nil)
 
 func (c *Ydb) CreateTable(tableName string) string {
-	return fmt.Sprintf(`
-		CREATE TABLE %s (
-			version_id Uint64,
-			is_applied Bool,
-			tstamp Timestamp,
-	
-			PRIMARY KEY(version_id)
-		);`,
-		tableName,
-	)
+	q := `CREATE TABLE %s (
+		version_id Uint64,
+		is_applied Bool,
+		tstamp Timestamp,
+
+		PRIMARY KEY(version_id)
+	)`
+	return fmt.Sprintf(q, tableName)
 }
 
 func (c *Ydb) InsertVersion(tableName string) string {
-	return fmt.Sprintf(`
-		UPSERT INTO %s (
-			version_id, 
-			is_applied, 
-			tstamp
-		) VALUES (
-			CAST($1 AS Uint64), 
-			$2, 
-			CurrentUtcTimestamp()
-		);`,
-		tableName,
-	)
+	q := `INSERT INTO %s (
+		version_id, 
+		is_applied, 
+		tstamp
+	) VALUES (
+		CAST($1 AS Uint64), 
+		$2, 
+		CurrentUtcTimestamp()
+	)`
+	return fmt.Sprintf(q, tableName)
 }
 
 func (c *Ydb) DeleteVersion(tableName string) string {
-	return fmt.Sprintf(`
-		DELETE FROM %s 
-		WHERE version_id = $1;`,
-		tableName,
-	)
+	q := `DELETE FROM %s WHERE version_id = $1`
+	return fmt.Sprintf(q, tableName)
 }
 
 func (c *Ydb) GetMigrationByVersion(tableName string) string {
-	return fmt.Sprintf(`
-		SELECT tstamp, is_applied 
-		FROM %s 
-		WHERE version_id = $1 
-		ORDER BY tstamp DESC LIMIT 1`,
-		tableName,
-	)
+	q := `SELECT tstamp, is_applied FROM %s WHERE version_id = $1 ORDER BY tstamp DESC LIMIT 1`
+	return fmt.Sprintf(q, tableName)
 }
 
 func (c *Ydb) ListMigrations(tableName string) string {
-	return fmt.Sprintf(`--!syntax_pg
-		SELECT version_id, is_applied
-		FROM %s 
-		ORDER BY tstamp DESC`,
-		tableName,
-	)
+	q := `--!syntax_pg
+	SELECT version_id, is_applied FROM %s ORDER BY tstamp DESC`
+	return fmt.Sprintf(q, tableName)
 }
