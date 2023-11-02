c6f78471dd82eacdbdb0a5df35dc86ce7717efae
Mike Fridman <mf192@icloud.com>
2023-11-02 11:48:24 -0400

    Add NewGoMigration constructor

--- globals_test.go
+++ globals_test.go
@@ -1,42 +1,196 @@
-package goose_test
+package goose
 
 import (
 	"context"
 	"database/sql"
 	"testing"
 
-	"github.com/pressly/goose/v3"
 	"github.com/pressly/goose/v3/internal/check"
 )
 
-func TestGlobalRegister(t *testing.T) {
-	// Avoid polluting other tests and do not run in parallel.
-	t.Cleanup(func() {
-		goose.ResetGlobalMigrations()
+func TestNewGoMigration(t *testing.T) {
+	t.Run("valid_both_nil", func(t *testing.T) {
+		m := NewGoMigration(1, nil, nil)
+		// roundtrip
+		check.Equal(t, m.Version, int64(1))
+		check.Equal(t, m.Type, TypeGo)
+		check.Equal(t, m.Registered, true)
+		check.Equal(t, m.Next, int64(-1))
+		check.Equal(t, m.Previous, int64(-1))
+		check.Equal(t, m.Source, "")
+		check.Bool(t, m.UpFnNoTxContext == nil, true)
+		check.Bool(t, m.DownFnNoTxContext == nil, true)
+		check.Bool(t, m.UpFnContext == nil, true)
+		check.Bool(t, m.DownFnContext == nil, true)
+		check.Bool(t, m.UpFn == nil, true)
+		check.Bool(t, m.DownFn == nil, true)
+		check.Bool(t, m.UpFnNoTx == nil, true)
+		check.Bool(t, m.DownFnNoTx == nil, true)
+		check.NotNil(t, m.goUp)
+		check.NotNil(t, m.goDown)
+		check.Equal(t, m.goUp.Mode, TransactionEnabled)
+		check.Equal(t, m.goDown.Mode, TransactionEnabled)
 	})
-	fnNoTx := func(context.Context, *sql.DB) error { return nil }
-	fn := func(context.Context, *sql.Tx) error { return nil }
+}
+
+func TestTransactionMode(t *testing.T) {
+	t.Cleanup(ResetGlobalMigrations)
+
+	runDB := func(context.Context, *sql.DB) error { return nil }
+	runTx := func(context.Context, *sql.Tx) error { return nil }
+
+	err := SetGlobalMigrations(
+		NewGoMigration(1, &GoFunc{RunTx: runTx, RunDB: runDB}, nil), // cannot specify both
+	)
+	check.HasError(t, err)
+	check.Contains(t, err.Error(), "up function: must specify exactly one of RunTx or RunDB")
+	err = SetGlobalMigrations(
+		NewGoMigration(1, nil, &GoFunc{RunTx: runTx, RunDB: runDB}), // cannot specify both
+	)
+	check.HasError(t, err)
+	check.Contains(t, err.Error(), "down function: must specify exactly one of RunTx or RunDB")
+	err = SetGlobalMigrations(
+		NewGoMigration(1, &GoFunc{RunTx: runTx, Mode: TransactionDisabled}, nil), // invalid explicit mode tx
+	)
+	check.HasError(t, err)
+	check.Contains(t, err.Error(), "up function: transaction mode must be enabled or unspecified when RunTx is set")
+	err = SetGlobalMigrations(
+		NewGoMigration(1, nil, &GoFunc{RunTx: runTx, Mode: TransactionDisabled}), // invalid explicit mode tx
+	)
+	check.HasError(t, err)
+	check.Contains(t, err.Error(), "down function: transaction mode must be enabled or unspecified when RunTx is set")
+	err = SetGlobalMigrations(
+		NewGoMigration(1, &GoFunc{RunDB: runDB, Mode: TransactionEnabled}, nil), // invalid explicit mode no-tx
+	)
+	check.HasError(t, err)
+	check.Contains(t, err.Error(), "up function: transaction mode must be disabled or unspecified when RunDB is set")
+	err = SetGlobalMigrations(
+		NewGoMigration(1, nil, &GoFunc{RunDB: runDB, Mode: TransactionEnabled}), // invalid explicit mode no-tx
+	)
+	check.HasError(t, err)
+	check.Contains(t, err.Error(), "down function: transaction mode must be disabled or unspecified when RunDB is set")
+
+	t.Run("default_mode", func(t *testing.T) {
+		t.Cleanup(ResetGlobalMigrations)
+
+		m := NewGoMigration(1, nil, nil)
+		err = SetGlobalMigrations(m)
+		check.NoError(t, err)
+		check.Number(t, len(registeredGoMigrations), 1)
+		registered := registeredGoMigrations[1]
+		check.NotNil(t, registered.goUp)
+		check.NotNil(t, registered.goDown)
+		check.Equal(t, registered.goUp.Mode, TransactionEnabled)
+		check.Equal(t, registered.goDown.Mode, TransactionEnabled)
+
+		migration2 := NewGoMigration(2, nil, nil)
+		// reset so we can check the default is set
+		migration2.goUp.Mode, migration2.goDown.Mode = 0, 0
+		err = SetGlobalMigrations(migration2)
+		check.NoError(t, err)
+		check.Number(t, len(registeredGoMigrations), 2)
+		registered = registeredGoMigrations[2]
+		check.NotNil(t, registered.goUp)
+		check.NotNil(t, registered.goDown)
+		check.Equal(t, registered.goUp.Mode, TransactionEnabled)
+		check.Equal(t, registered.goDown.Mode, TransactionEnabled)
+	})
+	t.Run("unknown_mode", func(t *testing.T) {
+		m := NewGoMigration(1, nil, nil)
+		m.goUp.Mode, m.goDown.Mode = 3, 3 // reset to default
+		err := SetGlobalMigrations(m)
+		check.HasError(t, err)
+		check.Contains(t, err.Error(), "invalid mode: 3")
+	})
+}
+
+func TestLegacyFunctions(t *testing.T) {
+	t.Cleanup(ResetGlobalMigrations)
+
+	runDB := func(context.Context, *sql.DB) error { return nil }
+	runTx := func(context.Context, *sql.Tx) error { return nil }
+
+	assertMigration := func(t *testing.T, m *Migration, version int64) {
+		t.Helper()
+		check.Equal(t, m.Version, version)
+		check.Equal(t, m.Type, TypeGo)
+		check.Equal(t, m.Registered, true)
+		check.Equal(t, m.Next, int64(-1))
+		check.Equal(t, m.Previous, int64(-1))
+		check.Equal(t, m.Source, "")
+	}
+
+	t.Run("all_tx", func(t *testing.T) {
+		err := SetGlobalMigrations(
+			NewGoMigration(1, &GoFunc{RunTx: runTx}, &GoFunc{RunTx: runTx}),
+		)
+		check.NoError(t, err)
+		check.Number(t, len(registeredGoMigrations), 1)
+		m := registeredGoMigrations[1]
+		assertMigration(t, m, 1)
+		// Legacy functions.
+		check.Bool(t, m.UpFnNoTxContext == nil, true)
+		check.Bool(t, m.DownFnNoTxContext == nil, true)
+		// Context-aware functions.
+		check.Bool(t, m.goUp == nil, false)
+		check.Bool(t, m.UpFnContext == nil, false)
+		check.Bool(t, m.goDown == nil, false)
+		check.Bool(t, m.DownFnContext == nil, false)
+		// Always nil
+		check.Bool(t, m.UpFn == nil, true)
+		check.Bool(t, m.DownFn == nil, true)
+		check.Bool(t, m.UpFnNoTx == nil, true)
+		check.Bool(t, m.DownFnNoTx == nil, true)
+	})
+	t.Run("all_db", func(t *testing.T) {
+		err := SetGlobalMigrations(
+			NewGoMigration(2, &GoFunc{RunDB: runDB}, &GoFunc{RunDB: runDB}),
+		)
+		check.NoError(t, err)
+		check.Number(t, len(registeredGoMigrations), 2)
+		m := registeredGoMigrations[2]
+		assertMigration(t, m, 2)
+		// Legacy functions.
+		check.Bool(t, m.UpFnNoTxContext == nil, false)
+		check.Bool(t, m.goUp == nil, false)
+		check.Bool(t, m.DownFnNoTxContext == nil, false)
+		check.Bool(t, m.goDown == nil, false)
+		// Context-aware functions.
+		check.Bool(t, m.UpFnContext == nil, true)
+		check.Bool(t, m.DownFnContext == nil, true)
+		// Always nil
+		check.Bool(t, m.UpFn == nil, true)
+		check.Bool(t, m.DownFn == nil, true)
+		check.Bool(t, m.UpFnNoTx == nil, true)
+		check.Bool(t, m.DownFnNoTx == nil, true)
+	})
+}
+
+func TestGlobalRegister(t *testing.T) {
+	t.Cleanup(ResetGlobalMigrations)
+
+	runDB := func(context.Context, *sql.DB) error { return nil }
+	runTx := func(context.Context, *sql.Tx) error { return nil }
 
 	// Success.
-	err := goose.SetGlobalMigrations(
-		[]goose.Migration{}...,
+	err := SetGlobalMigrations([]Migration{}...)
+	check.NoError(t, err)
+	err = SetGlobalMigrations(
+		NewGoMigration(1, &GoFunc{RunTx: runTx}, nil),
 	)
 	check.NoError(t, err)
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, Type: goose.TypeGo, UpFnContext: fn},
-	)
-	check.NoError(t, err)
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, Type: goose.TypeGo},
+	// Try to register the same migration again.
+	err = SetGlobalMigrations(
+		NewGoMigration(1, &GoFunc{RunTx: runTx}, nil),
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "go migration with version 1 already registered")
-	err = goose.SetGlobalMigrations(
-		goose.Migration{
+	err = SetGlobalMigrations(
+		Migration{
 			Registered:        true,
 			Version:           2,
 			Source:            "00002_foo.sql",
-			Type:              goose.TypeGo,
+			Type:              TypeGo,
 			UpFnContext:       func(context.Context, *sql.Tx) error { return nil },
 			DownFnNoTxContext: func(context.Context, *sql.DB) error { return nil },
 		},
@@ -44,69 +198,73 @@ func TestGlobalRegister(t *testing.T) {
 	check.NoError(t, err)
 	// Reset.
 	{
-		goose.ResetGlobalMigrations()
+		ResetGlobalMigrations()
 	}
 	// Failure.
-	err = goose.SetGlobalMigrations(
-		goose.Migration{},
+	err = SetGlobalMigrations(
+		Migration{},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "invalid go migration: must be registered")
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true},
+	err = SetGlobalMigrations(
+		Migration{Registered: true},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), `invalid go migration: type must be "go"`)
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, Type: goose.TypeSQL},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, Type: TypeSQL},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), `invalid go migration: type must be "go"`)
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 0, Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 0, Type: TypeGo},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "invalid go migration: version must be greater than zero")
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, Source: "2_foo.sql", Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, Source: "2_foo.sql", Type: TypeGo},
 	)
 	check.HasError(t, err)
-	check.Contains(t, err.Error(), `invalid go migration: numeric component [1] in go migration does not match version in source "2_foo.sql"`)
+	check.Contains(
+		t,
+		err.Error(),
+		`invalid go migration: version:1 does not match numeric component in source "2_foo.sql"`,
+	)
 	// Legacy functions.
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, UpFn: func(tx *sql.Tx) error { return nil }, Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, UpFn: func(tx *sql.Tx) error { return nil }, Type: TypeGo},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "invalid go migration: must not specify UpFn")
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, DownFn: func(tx *sql.Tx) error { return nil }, Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, DownFn: func(tx *sql.Tx) error { return nil }, Type: TypeGo},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "invalid go migration: must not specify DownFn")
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, UpFnNoTx: func(db *sql.DB) error { return nil }, Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, UpFnNoTx: func(db *sql.DB) error { return nil }, Type: TypeGo},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "invalid go migration: must not specify UpFnNoTx")
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, DownFnNoTx: func(db *sql.DB) error { return nil }, Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, DownFnNoTx: func(db *sql.DB) error { return nil }, Type: TypeGo},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "invalid go migration: must not specify DownFnNoTx")
 	// Context-aware functions.
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, UpFnContext: fn, UpFnNoTxContext: fnNoTx, Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, UpFnContext: runTx, UpFnNoTxContext: runDB, Type: TypeGo},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "invalid go migration: must specify exactly one of UpFnContext or UpFnNoTxContext")
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, DownFnContext: fn, DownFnNoTxContext: fnNoTx, Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, DownFnContext: runTx, DownFnNoTxContext: runDB, Type: TypeGo},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), "invalid go migration: must specify exactly one of DownFnContext or DownFnNoTxContext")
 	// Source and version mismatch.
-	err = goose.SetGlobalMigrations(
-		goose.Migration{Registered: true, Version: 1, Source: "invalid_numeric.sql", Type: goose.TypeGo},
+	err = SetGlobalMigrations(
+		Migration{Registered: true, Version: 1, Source: "invalid_numeric.sql", Type: TypeGo},
 	)
 	check.HasError(t, err)
 	check.Contains(t, err.Error(), `invalid go migration: failed to parse version from migration file: invalid_numeric.sql`)
--- migration.go
+++ migration.go
@@ -13,34 +13,111 @@ import (
 	"github.com/pressly/goose/v3/internal/sqlparser"
 )
 
+// NewGoMigration creates a new Go migration.
+//
+// Both up and down functions may be nil, in which case the migration will be recorded in the
+// versions table but no functions will be run. This is useful for recording (up) or deleting (down)
+// a version without running any functions. See [GoFunc] for more details.
+func NewGoMigration(version int64, up, down *GoFunc) Migration {
+	m := Migration{
+		Type:       TypeGo,
+		Registered: true,
+		Version:    version,
+		Next:       -1,
+		Previous:   -1,
+		goUp:       &GoFunc{Mode: TransactionEnabled},
+		goDown:     &GoFunc{Mode: TransactionEnabled},
+	}
+	if up != nil {
+		m.goUp = up
+	}
+	if down != nil {
+		m.goDown = down
+	}
+	return m
+}
+
+// Migration struct represents either a SQL or Go migration.
+//
+// Avoid constructing migrations manually, use [NewGoMigration] function.
+type Migration struct {
+	Type    MigrationType
+	Version int64
+	// Source is the path to the .sql script or .go file. It may be empty for Go migrations that
+	// have been registered globally and don't have a source file.
+	Source string
+
+	UpFnContext, DownFnContext         GoMigrationContext
+	UpFnNoTxContext, DownFnNoTxContext GoMigrationNoTxContext
+	// These fields are used internally by goose and users are not expected to set them. Instead,
+	// use [NewGoMigration] to create a new go migration.
+	goUp, goDown *GoFunc
+
+	// These fields will be removed in a future major version. They are here for backwards
+	// compatibility and are an implementation detail.
+	Registered bool
+	UseTx      bool
+	Next       int64 // next version, or -1 if none
+	Previous   int64 // previous version, -1 if none
+
+	// We still save the non-context versions in the struct in case someone is using them. Goose
+	// does not use these internally anymore in favor of the context-aware versions. These fields
+	// will be removed in a future major version.
+
+	UpFn       GoMigration     // Deprecated: use UpFnContext instead.
+	DownFn     GoMigration     // Deprecated: use DownFnContext instead.
+	UpFnNoTx   GoMigrationNoTx // Deprecated: use UpFnNoTxContext instead.
+	DownFnNoTx GoMigrationNoTx // Deprecated: use DownFnNoTxContext instead.
+
+	noVersioning bool
+}
+
+// GoFunc represents a Go migration function.
+type GoFunc struct {
+	// Exactly one of these must be set, or both must be nil.
+	RunTx func(ctx context.Context, tx *sql.Tx) error
+	// -- OR --
+	RunDB func(ctx context.Context, db *sql.DB) error
+
+	// Mode is the transaction mode for the migration. When one of the run functions is set, the
+	// mode will be inferred from the function and the field is ignored. Users do not need to set
+	// this field when supplying a run function.
+	//
+	// If both run functions are nil, the mode defaults to TransactionEnabled. The use case for nil
+	// functions is to record a version in the version table without invoking a Go migration
+	// function.
+	//
+	// The only time this field is required is if BOTH run functions are nil AND you want to
+	// override the default transaction mode.
+	Mode TransactionMode
+}
+
+// TransactionMode represents the possible transaction modes for a migration.
+type TransactionMode int
+
+const (
+	TransactionEnabled TransactionMode = iota + 1
+	TransactionDisabled
+)
+
+func (m TransactionMode) String() string {
+	switch m {
+	case TransactionEnabled:
+		return "transaction_enabled"
+	case TransactionDisabled:
+		return "transaction_disabled"
+	default:
+		return fmt.Sprintf("unknown transaction mode (%d)", m)
+	}
+}
+
 // MigrationRecord struct.
+//
+// Deprecated: unused and will be removed in a future major version.
 type MigrationRecord struct {
 	VersionID int64
 	TStamp    time.Time
 	IsApplied bool // was this a result of up() or down()
-}
-
-// Migration struct represents either a SQL or Go migration.
-type Migration struct {
-	Type                               MigrationType
-	Version                            int64
-	Source                             string // path to .sql script or .go file
-	Registered                         bool
-	UpFnContext, DownFnContext         GoMigrationContext
-	UpFnNoTxContext, DownFnNoTxContext GoMigrationNoTxContext
-
-	// These fields will be removed in a future major version. They are here for backwards
-	// compatibility and are an implementation detail.
-	UseTx    bool
-	Next     int64 // next version, or -1 if none
-	Previous int64 // previous version, -1 if none
-
-	// We still save the non-context versions in the struct in case someone is using them. Goose
-	// does not use these internally anymore in favor of the context-aware versions.
-	UpFn, DownFn         GoMigration
-	UpFnNoTx, DownFnNoTx GoMigrationNoTx
-
-	noVersioning bool
 }
 
 func (m *Migration) String() string {
@@ -254,4 +331,116 @@ func truncateDuration(d time.Duration) time.Duration {
 		}
 	}
 	return d
+}
+
+func verifyAndUpdateGoFunc(f *GoFunc) error {
+	if f == nil {
+		return nil
+	}
+	if f.RunTx != nil && f.RunDB != nil {
+		return errors.New("must specify exactly one of RunTx or RunDB")
+	}
+	if f.RunTx == nil && f.RunDB == nil {
+		switch f.Mode {
+		case 0:
+			// Default to TransactionEnabled ONLY if mode is not set explicitly.
+			f.Mode = TransactionEnabled
+		case TransactionEnabled, TransactionDisabled:
+			// No functions but mode is set. This is not an error. It means the user wants to record
+			// a version with the given mode but not run any functions.
+		default:
+			return fmt.Errorf("invalid mode: %d", f.Mode)
+		}
+		return nil
+	}
+	if f.RunDB != nil {
+		switch f.Mode {
+		case 0, TransactionDisabled:
+			f.Mode = TransactionDisabled
+		default:
+			return fmt.Errorf("transaction mode must be disabled or unspecified when RunDB is set")
+		}
+	}
+	if f.RunTx != nil {
+		switch f.Mode {
+		case 0, TransactionEnabled:
+			f.Mode = TransactionEnabled
+		default:
+			return fmt.Errorf("transaction mode must be enabled or unspecified when RunTx is set")
+		}
+	}
+	// This is a defensive check. If the mode is still 0, it means we failed to infer the mode from
+	// the functions or return an error. This should never happen.
+	if f.Mode == 0 {
+		return errors.New("failed to infer transaction mode")
+	}
+	return nil
+}
+
+func updateLegacyFuncs(m *Migration) error {
+	// Assign the context-aware functions to the legacy functions. This is an implementation detail
+	// and will be removed in a future major version. Users are encouraged to use [NewGoMigration]
+	// instead of constructing a Migration struct directly.
+	if up := m.goUp; up != nil {
+		if up.RunTx != nil {
+			m.UpFnContext = up.RunTx
+			m.UseTx = true
+		}
+		if up.RunDB != nil {
+			m.UpFnNoTxContext = up.RunDB
+		}
+	}
+	if down := m.goDown; down != nil {
+		if down.RunTx != nil {
+			m.DownFnContext = down.RunTx
+			m.UseTx = true
+		}
+		if down.RunDB != nil {
+			m.DownFnNoTxContext = down.RunDB
+		}
+	}
+	if m.UpFnContext != nil && m.UpFnNoTxContext != nil {
+		return errors.New("must specify exactly one of UpFnContext or UpFnNoTxContext")
+	}
+	if m.DownFnContext != nil && m.DownFnNoTxContext != nil {
+		return errors.New("must specify exactly one of DownFnContext or DownFnNoTxContext")
+	}
+	// Do not allow legacy functions to be set.
+	if m.UpFn != nil {
+		return errors.New("must not specify UpFn")
+	}
+	if m.DownFn != nil {
+		return errors.New("must not specify DownFn")
+	}
+	if m.UpFnNoTx != nil {
+		return errors.New("must not specify UpFnNoTx")
+	}
+	if m.DownFnNoTx != nil {
+		return errors.New("must not specify DownFnNoTx")
+	}
+	return nil
+}
+
+func validGoMigration(m *Migration) error {
+	if !m.Registered {
+		return errors.New("must be registered")
+	}
+	if m.Type != TypeGo {
+		return fmt.Errorf("type must be %q", TypeGo)
+	}
+	if m.Version < 1 {
+		return errors.New("version must be greater than zero")
+	}
+	if m.Source != "" {
+		// If the source is set, expect it to be a path with a numeric component that matches the
+		// version. This field is not intended to be used for descriptive purposes.
+		version, err := NumericComponent(m.Source)
+		if err != nil {
+			return err
+		}
+		if version != m.Version {
+			return fmt.Errorf("version:%d does not match numeric component in source %q", m.Version, m.Source)
+		}
+	}
+	return nil
 }
