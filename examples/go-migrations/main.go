26419829996f2b36a290da89184141875862ef75
Adam Björnberg <adam.bjornberg@leica-geosystems.com>
2023-11-21 22:04:17 +0100

    update custom goose binary example

--- examples/go-migrations/main.go
+++ examples/go-migrations/main.go
@@ -3,47 +3,11 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"os"
-
 	"github.com/pressly/goose/v3"
-	_ "modernc.org/sqlite"
-)
-
-var (
-	flags = flag.NewFlagSet("goose", flag.ExitOnError)
-	dir   = flags.String("dir", ".", "directory with migration files")
+	"os"
 )
 
 func main() {
-	flags.Parse(os.Args[1:])
-	args := flags.Args()
-
-	if len(args) < 3 {
-		flags.Usage()
-		return
-	}
-
-	dbstring, command := args[1], args[2]
-
-	db, err := goose.OpenDBWithDriver("sqlite", dbstring)
-	if err != nil {
-		log.Fatalf("goose: failed to open DB: %v\n", err)
-	}
-
-	defer func() {
-		if err := db.Close(); err != nil {
-			log.Fatalf("goose: failed to close DB: %v\n", err)
-		}
-	}()
-
-	arguments := []string{}
-	if len(args) > 3 {
-		arguments = append(arguments, args[3:]...)
-	}
-
-	if err := goose.Run(command, db, *dir, arguments...); err != nil {
-		log.Fatalf("goose %v: %v", command, err)
-	}
+	os.Args = append([]string{os.Args[0], "sqlite3"}, os.Args[1:]...)
+	goose.Main()
 }
