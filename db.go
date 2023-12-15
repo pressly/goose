6e43ae92847b6ee5bae964457f21ab18d95a5a54
vpetrosian <91080025+vpetrosian@users.noreply.github.com>
2023-12-15 07:46:35 +0400

    fix: redshift driver issue (#567)
    Co-authored-by: Mike Fridman <mf192@icloud.com>

--- db.go
+++ db.go
@@ -15,8 +15,6 @@ func OpenDBWithDriver(driver string, dbstring string) (*sql.DB, error) {
 	switch driver {
 	case "mssql":
 		driver = "sqlserver"
-	case "redshift":
-		driver = "postgres"
 	case "tidb":
 		driver = "mysql"
 	}
