1740b96e382ea6e1f5247be470fd0863bcaa8c39
Mike Fridman <mf192@icloud.com>
2023-12-22 13:38:16 -0500

    add test case

--- internal/sqlparser/testdata/envsub/test02/input.sql
+++ internal/sqlparser/testdata/envsub/test02/input.sql
@@ -9,3 +9,12 @@ CREATE TABLE post (
 	${ANOTHER_VAR:-default}title4 text,
 	${GOOSE_ENV_SET_BUT_EMPTY_VALUE-default}title5 text,
 );
+
+-- +goose StatementBegin
+CREATE OR REPLACE FUNCTION test_func()
+RETURNS void AS $$
+BEGIN
+	RAISE NOTICE '${GOOSE_ENV_NAME}';
+END;
+$$ LANGUAGE plpgsql;
+-- +goose StatementEnd
