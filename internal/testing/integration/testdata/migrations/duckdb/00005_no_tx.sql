19e067bbe20b5df1d725e8a7c789abf5e9ce5527
Mike Fridman <mf192@icloud.com>
2024-03-14 09:01:51 -0400

    wip

--- internal/testing/integration/testdata/migrations/duckdb/00005_no_tx.sql
+++
@@ -1,9 +0,0 @@
--- +goose NO TRANSACTION
-
--- +goose Up
-CREATE UNIQUE INDEX owners_idx ON owners (owner_name);
-
--- +goose Down
--- +goose StatementBegin
-DROP INDEX owners_idx;
--- +goose StatementEnd
