2152a37630450d2eb98dd0fcfa87f4c48ada6263
Mike Fridman <mf192@icloud.com>
2024-04-11 21:08:29 -0400

    improve comments

--- database/store.go
+++ database/store.go
@@ -16,32 +16,26 @@ var (
 // implementations can be created to support different databases without reimplementing the
 // migration logic.
 //
-// This package exposes a number of dialects that implement the Store interface. Most users will not
-// need to implement their own Store, but if you need to support a database that is not supported,
-// you can implement your own.
+// This package provides several dialects that implement the Store interface. While most users won't
+// need to create their own Store, if you need to support a database that isn't currently supported,
+// you can implement your own!
 type Store interface {
 	// Tablename is the name of the version table. This table is used to record applied migrations
 	// and must not be an empty string.
 	Tablename() string
-
 	// CreateVersionTable creates the version table, which is used to track migrations. When
 	// creating this table, the implementation MUST also insert a row for the initial version (0).
 	CreateVersionTable(ctx context.Context, db DBTxConn) error
-
 	// Insert a version id into the version table.
 	Insert(ctx context.Context, db DBTxConn, req InsertRequest) error
-
 	// Delete a version id from the version table.
 	Delete(ctx context.Context, db DBTxConn, version int64) error
-
 	// GetMigration retrieves a single migration by version id. If the query succeeds, but the
 	// version is not found, this method must return [ErrVersionNotFound].
 	GetMigration(ctx context.Context, db DBTxConn, version int64) (*GetMigrationResult, error)
-
 	// GetLatestVersion retrieves the last applied migration version. If no migrations exist, this
 	// method must return -1 and no error.
 	GetLatestVersion(ctx context.Context, db DBTxConn) (int64, error)
-
 	// ListMigrations retrieves all migrations sorted in descending order by id or timestamp. If
 	// there are no migrations, return empty slice with no error. Typically this method will return
 	// at least one migration, because the initial version (0) is always inserted into the version
