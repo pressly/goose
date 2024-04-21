7863f54bd5408c92bc3d980b6c5c610e25696082
Mike Fridman <mf192@icloud.com>
2024-04-20 22:42:42 -0400

    docs

--- internal/dialect/dialectquery/dialectquery.go
+++ internal/dialect/dialectquery/dialectquery.go
@@ -1,27 +1,22 @@
 package dialectquery
 
-// Querier is the interface that wraps the basic methods to create a dialect
-// specific query.
+// Querier is the interface that wraps the basic methods to create a dialect specific query.
 type Querier interface {
 	// CreateTable returns the SQL query string to create the db version table.
 	CreateTable(tableName string) string
 
-	// InsertVersion returns the SQL query string to insert a new version into
-	// the db version table.
+	// InsertVersion returns the SQL query string to insert a new version into the db version table.
 	InsertVersion(tableName string) string
 
-	// DeleteVersion returns the SQL query string to delete a version from
-	// the db version table.
+	// DeleteVersion returns the SQL query string to delete a version from the db version table.
 	DeleteVersion(tableName string) string
 
-	// GetMigrationByVersion returns the SQL query string to get a single
-	// migration by version.
+	// GetMigrationByVersion returns the SQL query string to get a single migration by version.
 	//
 	// The query should return the timestamp and is_applied columns.
 	GetMigrationByVersion(tableName string) string
 
-	// ListMigrations returns the SQL query string to list all migrations in
-	// descending order by id.
+	// ListMigrations returns the SQL query string to list all migrations in descending order by id.
 	//
 	// The query should return the version_id and is_applied columns.
 	ListMigrations(tableName string) string
@@ -38,6 +33,10 @@ func NewQueryController(querier Querier) *QueryController {
 
 // Optional methods
 
+// TableExists returns the SQL query string to check if the version table exists. If the Querier
+// does not implement this method, it will return an empty string.
+//
+// The query should return a boolean value.
 func (c *QueryController) TableExists(tableName string) string {
 	if t, ok := c.querier.(interface {
 		TableExists(string) string
