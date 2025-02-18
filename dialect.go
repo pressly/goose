04b3ffa7280a4392f245edab5eca301890535f06
Viktor Alenkov <viktor@alenkov.name>
2025-02-18 08:14:12 +0300

    Dialect refactoring
    1. Published a method for getting dialect
    2. Created a public error

--- dialect.go
+++ dialect.go
@@ -2,26 +2,29 @@ package goose
 
 import (
 	"fmt"
+	"strings"
 
-	"github.com/pressly/goose/v3/database"
 	"github.com/pressly/goose/v3/internal/dialect"
 )
 
-// Dialect is the type of database dialect. It is an alias for [database.Dialect].
-type Dialect = database.Dialect
+// Dialect is the type of database dialect. It is an alias for [dialect.Dialect].
+type Dialect = dialect.Dialect
 
 const (
-	DialectClickHouse Dialect = database.DialectClickHouse
-	DialectMSSQL      Dialect = database.DialectMSSQL
-	DialectMySQL      Dialect = database.DialectMySQL
-	DialectPostgres   Dialect = database.DialectPostgres
-	DialectRedshift   Dialect = database.DialectRedshift
-	DialectSQLite3    Dialect = database.DialectSQLite3
-	DialectTiDB       Dialect = database.DialectTiDB
-	DialectVertica    Dialect = database.DialectVertica
-	DialectYdB        Dialect = database.DialectYdB
-	DialectStarrocks  Dialect = database.DialectStarrocks
+	DialectClickHouse Dialect = dialect.Clickhouse
+	DialectMSSQL      Dialect = dialect.Mssql
+	DialectMySQL      Dialect = dialect.Mysql
+	DialectPostgres   Dialect = dialect.Postgres
+	DialectRedshift   Dialect = dialect.Redshift
+	DialectSQLite3    Dialect = dialect.Sqlite3
+	DialectTiDB       Dialect = dialect.Tidb
+	DialectVertica    Dialect = dialect.Vertica
+	DialectYdB        Dialect = dialect.Ydb
+	DialectTurso      Dialect = dialect.Turso
+	DialectStarrocks  Dialect = dialect.Starrocks
 )
+
+var ErrUnknownDialect = dialect.ErrUnknownDialect
 
 func init() {
 	store, _ = dialect.NewStore(dialect.Postgres)
@@ -30,35 +33,52 @@ func init() {
 var store dialect.Store
 
 // SetDialect sets the dialect to use for the goose package.
-func SetDialect(s string) error {
-	var d dialect.Dialect
-	switch s {
-	case "postgres", "pgx":
-		d = dialect.Postgres
-	case "mysql":
-		d = dialect.Mysql
-	case "sqlite3", "sqlite":
-		d = dialect.Sqlite3
-	case "mssql", "azuresql", "sqlserver":
-		d = dialect.Sqlserver
-	case "redshift":
-		d = dialect.Redshift
-	case "tidb":
-		d = dialect.Tidb
-	case "clickhouse":
-		d = dialect.Clickhouse
-	case "vertica":
-		d = dialect.Vertica
-	case "ydb":
-		d = dialect.Ydb
-	case "turso":
-		d = dialect.Turso
-	case "starrocks":
-		d = dialect.Starrocks
-	default:
-		return fmt.Errorf("%q: unknown dialect", s)
+func SetDialect[D string | Dialect](d D) error {
+	var (
+		v   Dialect
+		err error
+	)
+
+	switch t := any(d).(type) {
+	case string:
+		v, err = GetDialect(t)
+		if err != nil {
+			return err
+		}
+	case Dialect:
+		v = t
 	}
-	var err error
-	store, err = dialect.NewStore(d)
+
+	store, err = dialect.NewStore(v)
 	return err
+}
+
+// GetDialect gets the dialect used in the goose package.
+func GetDialect(s string) (Dialect, error) {
+	switch strings.ToLower(s) {
+	case "postgres", "pgx":
+		return DialectPostgres, nil
+	case "mysql":
+		return DialectMySQL, nil
+	case "sqlite3", "sqlite":
+		return DialectSQLite3, nil
+	case "mssql", "azuresql", "sqlserver":
+		return DialectMSSQL, nil
+	case "redshift":
+		return DialectRedshift, nil
+	case "tidb":
+		return DialectTiDB, nil
+	case "clickhouse":
+		return DialectClickHouse, nil
+	case "vertica":
+		return DialectVertica, nil
+	case "ydb":
+		return DialectYdB, nil
+	case "turso":
+		return DialectTurso, nil
+	case "starrocks":
+		return DialectStarrocks, nil
+	default:
+		return "", fmt.Errorf("%s: %w", s, ErrUnknownDialect)
+	}
 }
--- internal/dialect/dialects.go
+++ internal/dialect/dialects.go
@@ -1,12 +1,18 @@
 package dialect
 
+import "errors"
+
 // Dialect is the type of database dialect.
 type Dialect string
 
+var ErrUnknownDialect = errors.New("unknown dialect")
+
 const (
-	Postgres   Dialect = "postgres"
-	Mysql      Dialect = "mysql"
-	Sqlite3    Dialect = "sqlite3"
+	Postgres Dialect = "postgres"
+	Mysql    Dialect = "mysql"
+	Sqlite3  Dialect = "sqlite3"
+	Mssql    Dialect = "mssql"
+	// Deprecated: use [Mssql]
 	Sqlserver  Dialect = "sqlserver"
 	Redshift   Dialect = "redshift"
 	Tidb       Dialect = "tidb"
