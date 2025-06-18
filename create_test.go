1f1ce2bdc98ee300acfa9cdeae2c29953d9da6c8
lwileczek <wileczek105@gmail.com>
2025-06-18 10:38:32 -0400

    Add flag to increment by arbitrary integer
    Allow users to increment sequential migrations by an arbitrary integer.
    This allows them to add migrations like network rules to add new
    migrations between existing ones

--- create_test.go
+++ create_test.go
@@ -15,38 +15,77 @@ func TestSequential(t *testing.T) {
 		t.Skip("skip long running test")
 	}
 
-	dir := t.TempDir()
-	defer os.Remove("./bin/create-goose") // clean up
+	t.Run("Create migrations with sequential keys", func(t *testing.T) {
+		dir := t.TempDir()
+		defer os.Remove("./bin/create-goose") // clean up
 
-	commands := []string{
-		"go build -o ./bin/create-goose ./cmd/goose",
-		fmt.Sprintf("./bin/create-goose -s -dir=%s create create_table", dir),
-		fmt.Sprintf("./bin/create-goose -s -dir=%s create add_users", dir),
-		fmt.Sprintf("./bin/create-goose -s -dir=%s create add_indices", dir),
-		fmt.Sprintf("./bin/create-goose -s -dir=%s create update_users", dir),
-	}
+		commands := []string{
+			"go build -o ./bin/create-goose ./cmd/goose",
+			fmt.Sprintf("./bin/create-goose -s -dir=%s create create_table", dir),
+			fmt.Sprintf("./bin/create-goose -s -dir=%s create add_users", dir),
+			fmt.Sprintf("./bin/create-goose -s -dir=%s create add_indices", dir),
+			fmt.Sprintf("./bin/create-goose -s -dir=%s create update_users", dir),
+		}
 
-	for _, cmd := range commands {
-		args := strings.Split(cmd, " ")
-		time.Sleep(1 * time.Second)
-		cmd := exec.Command(args[0], args[1:]...)
-		cmd.Env = os.Environ()
-		out, err := cmd.CombinedOutput()
+		for _, cmd := range commands {
+			args := strings.Split(cmd, " ")
+			time.Sleep(1 * time.Second)
+			cmd := exec.Command(args[0], args[1:]...)
+			cmd.Env = os.Environ()
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatalf("%s:\n%v\n\n%s", err, cmd, out)
+			}
+		}
+
+		files, err := os.ReadDir(dir)
 		if err != nil {
-			t.Fatalf("%s:\n%v\n\n%s", err, cmd, out)
+			t.Fatal(err)
 		}
-	}
 
-	files, err := os.ReadDir(dir)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// check that the files are in order
-	for i, f := range files {
-		expected := fmt.Sprintf("%05v", i+1)
-		if !strings.HasPrefix(f.Name(), expected) {
-			t.Errorf("failed to find %s prefix in %s", expected, f.Name())
+		// check that the files are in order
+		for i, f := range files {
+			expected := fmt.Sprintf("%05v", i+1)
+			if !strings.HasPrefix(f.Name(), expected) {
+				t.Errorf("failed to find %s prefix in %s", expected, f.Name())
+			}
 		}
-	}
+	})
+
+	t.Run("Increment by 10", func(t *testing.T) {
+		inc := 10
+		dir := t.TempDir()
+		defer os.Remove("./bin/create-goose") // clean up
+		commands := []string{
+			"go build -o ./bin/create-goose ./cmd/goose",
+			fmt.Sprintf("./bin/create-goose -s -inc %d -dir=%s create create_table", inc, dir),
+			fmt.Sprintf("./bin/create-goose -s -inc %d -dir=%s create add_users", inc, dir),
+			fmt.Sprintf("./bin/create-goose -s -inc %d -dir=%s create add_indices", inc, dir),
+			fmt.Sprintf("./bin/create-goose -s -inc %d -dir=%s create update_users", inc, dir),
+		}
+
+		for _, cmd := range commands {
+			args := strings.Split(cmd, " ")
+			time.Sleep(1 * time.Second)
+			cmd := exec.Command(args[0], args[1:]...)
+			cmd.Env = os.Environ()
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				t.Fatalf("%s:\n%v\n\n%s", err, cmd, out)
+			}
+		}
+
+		files, err := os.ReadDir(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// check that the files are in order
+		for i, f := range files {
+			expected := fmt.Sprintf("%05v", i+inc)
+			if !strings.HasPrefix(f.Name(), expected) {
+				t.Errorf("failed to find %s prefix in %s", expected, f.Name())
+			}
+		}
+	})
 }
