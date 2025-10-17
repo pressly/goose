03ff6722035b4b3a0afef1eb9fabf24c18269924
Ben Ellis <bellis@fubo.tv>
2025-10-17 12:09:56 -0400

    Sort output.

--- status.go
+++ status.go
@@ -5,8 +5,40 @@ import (
 	"database/sql"
 	"fmt"
 	"path/filepath"
+	"sort"
 	"time"
+
+	"github.com/pressly/goose/v3/internal/legacystore"
 )
+
+type statusLine struct {
+	Version   int64
+	AppliedAt time.Time
+	Pending   bool
+	Source    string
+}
+
+type statusLines []*statusLine
+
+// helpers so we can use pkg sort
+func (s statusLines) Len() int      { return len(s) }
+func (s statusLines) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s statusLines) Less(i, j int) bool {
+	lineI := s[i]
+	lineJ := s[j]
+	// Pending migrations always come later:
+	if lineI.Pending != lineJ.Pending {
+		// lineJ is pending ---> lineI goes first and return value true means lineI < lineJ
+		return lineJ.Pending
+	}
+	if !lineI.AppliedAt.Equal(lineJ.AppliedAt) {
+		return lineI.AppliedAt.Before(lineJ.AppliedAt)
+	}
+	if lineI.Version != lineJ.Version {
+		return lineI.Version < lineJ.Version
+	}
+	return lineI.Source < lineJ.Source
+}
 
 // Status prints the status of all migrations.
 func Status(db *sql.DB, dir string, opts ...OptionsFunc) error {
@@ -20,14 +52,14 @@ func StatusContext(ctx context.Context, db *sql.DB, dir string, opts ...OptionsF
 	for _, f := range opts {
 		f(option)
 	}
-	migrations, err := CollectMigrations(dir, minVersion, maxVersion)
+	fsMigrations, err := CollectMigrations(dir, minVersion, maxVersion)
 	if err != nil {
 		return fmt.Errorf("failed to collect migrations: %w", err)
 	}
 	if option.noVersioning {
 		log.Printf("    Applied At                  Migration")
 		log.Printf("    =======================================")
-		for _, current := range migrations {
+		for _, current := range fsMigrations {
 			log.Printf("    %-24s -- %v", "no versioning", filepath.Base(current.Source))
 		}
 		return nil
@@ -44,29 +76,37 @@ func StatusContext(ctx context.Context, db *sql.DB, dir string, opts ...OptionsF
 		return fmt.Errorf("failed to list migrations: %w", err)
 	}
 
-	// Build a map of version_id to migration result for quick lookup
-	dbMigrationMap := make(map[int64]*struct {
-		Timestamp time.Time
-		IsApplied bool
-	})
+	// Build a map on version_id to match migrations in DB to migrations from FS.
+	dbMigrationMap := make(map[int64]*legacystore.ListMigrationsResult)
 	for _, m := range dbMigrations {
-		dbMigrationMap[m.VersionID] = &struct {
-			Timestamp time.Time
-			IsApplied bool
-		}{
-			Timestamp: m.Timestamp,
-			IsApplied: m.IsApplied,
-		}
+		dbMigrationMap[m.VersionID] = m
 	}
+
+	// Gather 1 status line for each migration in the FS, enriched with application timestamp from DB if applied:
+	var statusOutput statusLines
+	for _, fsM := range fsMigrations {
+		line := statusLine{
+			Version:   fsM.Version,
+			AppliedAt: time.Time{},
+			Pending:   true,
+			Source:    filepath.Base(fsM.Source),
+		}
+		if dbM, exists := dbMigrationMap[fsM.Version]; exists && dbM.IsApplied {
+			line.Pending = false
+			line.AppliedAt = dbM.Timestamp
+		}
+		statusOutput = append(statusOutput, &line)
+	}
+	sort.Sort(statusOutput)
 
 	log.Printf("    Applied At                  Migration")
 	log.Printf("    =======================================")
-	for _, migration := range migrations {
+	for _, migration := range statusOutput {
 		appliedAt := "Pending"
-		if m, exists := dbMigrationMap[migration.Version]; exists && m.IsApplied {
-			appliedAt = m.Timestamp.Format(time.ANSIC)
+		if !migration.Pending {
+			appliedAt = migration.AppliedAt.Format(time.ANSIC)
 		}
-		log.Printf("    %-24s -- %v", appliedAt, filepath.Base(migration.Source))
+		log.Printf("    %-24s -- %v", appliedAt, migration.Source)
 	}
 
 	return nil
